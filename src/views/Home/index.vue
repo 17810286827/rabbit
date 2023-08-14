5b94fa997dacb76ccd3a6929ff097e1aa0c12e96
17810286827 <1303850347@qq.com>
2023-08-14 15:58:52 +0800

    pannel组件完成封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,14 @@
 <script setup>
   import homeCategory from '@/views/Home/components/homeCategory.vue'
+  import homeBanner from '@/views/Home/components/homeBanner.vue'
+  import homePannel from '@/views/Home/components/homePannel.vue'
 </script>
 
 <template>
   <div class="container">
     <homeCategory></homeCategory>
+    <homeBanner></homeBanner>
+    <homePannel title="111" subTitle="22222"> askldnakosdjako </homePannel>
+    <homePannel title="12313" subTitle="123123">klajsdopasf</homePannel>
   </div>
 </template>
